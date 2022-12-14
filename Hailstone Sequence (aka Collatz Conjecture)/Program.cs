afc80baff29a9aab8c9f086952ad09cad4445224
Beyaz1200 <113636921+Beyaz1200@users.noreply.github.com>
2022-12-14 19:39:44 +0100

    Add files via upload

---
+++ Hailstone Sequence (aka Collatz Conjecture)/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Type a number to start the vlaue processing");
+int i  = int.Parse(Console.ReadLine()!);
+
+while (i != 1)
+{
+    if (i % 2 == 0) {i= i /2; Console.Write("" + i + " ");}
+    else {i = (3 * i)+ 1;   Console.Write("" + i + " ");}
+}
---
+++ Math Quiz/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear();
+string Operator = "0";
+double operand1= 0;
+double operand2 = 0;
+double result = 0 ;
+
+Console.Write("Chosse one the numbers: ");
+operand1 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Chosse one the numbers: ");
+operand2 = double.Parse(Console.ReadLine()!);
+
+Console.Write("The result is: ");
+result = double.Parse(Console.ReadLine()!);
+
+if (operand1 + operand2 == result)
+{
+Operator = "+";
+}
+
+else if (operand1 - operand2 == result)
+{
+Operator = "-";
+}
+
+else if (operand2 - operand1 == result)
+{
+Operator = "-";
+}
+
+else if (operand2 * operand1 == result)
+{
+Operator = "*";
+}
+
+else if (operand2 / operand1 == result)
+{
+Operator = "/";
+}
+
+else if (operand1 / operand2 == result)
+{
+Operator = "/";
+}
+
+Console.Write($"The operarot is {Operator}");
+
